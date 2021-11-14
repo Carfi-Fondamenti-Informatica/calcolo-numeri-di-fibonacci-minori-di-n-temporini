6c9be4b6fb28dd0bdf74c22565d4ea7b6e787aff
temporini <91788264+temporini@users.noreply.github.com>
2021-11-14 21:45:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
-}
+    int main() {
+
+        int n = 0, a = 0, b = 1,  c=0;
+
+        cin >> n;
+
+       
+            cout <<a<<b<<endl;
+            for (int i=0 ; i < (n-1); i++){
+              
+                c = a + b;
+            cout << c << endl;
+            a = b;
+            b = c;
+        }
+       
+        return 0;
+
+    }
