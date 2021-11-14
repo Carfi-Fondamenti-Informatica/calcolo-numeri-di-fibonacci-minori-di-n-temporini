2022ae88d95946b602f05a5fde29ffbaf43d5fb3
temporini <91788264+temporini@users.noreply.github.com>
2021-11-14 21:46:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
        
             cout <<a<<b<<endl;
-            for (int i=0 ; i < (n-1); i++){
+            for (int i=0 ; i < n; i++){
               
                 c = a + b;
             cout << c << endl;
